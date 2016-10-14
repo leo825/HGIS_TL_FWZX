66886befdb52e1d7b84bed199092d42cba5a3fe1
leo825 <294864460@qq.com>
2016-10-14 09:03:49 +0800

    1.修改了输出方式，将之前的输出改成了直接输出字节流的方式，避免了

--- src/main/java/cn/geobeans/fwzx/util/Pagination.java
+++ src/main/java/cn/geobeans/fwzx/util/Pagination.java
@@ -7,6 +7,8 @@ package cn.geobeans.fwzx.util;
  */
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -99,7 +101,42 @@ public class Pagination implements Serializable {
         paginationSQL.append(" ) where　rownum_ > " + startIndex);
 
         // 装入结果集（key转为小写）
-        setRows(StringUtil.listKeyToLower(jdbcTemplate.queryForList(paginationSQL.toString())));
+        setRows(listKeyToLower(jdbcTemplate.queryForList(paginationSQL.toString())));
+    }
+
+
+    /**
+     * 把list map中map对象的key全部转为小写形式
+     *
+     * @param listmap
+     * @return
+     */
+    public static List<Map<String, Object>> listKeyToLower(List<Map<String, Object>> listmap) {
+        List<Map<String, Object>> r = new ArrayList<Map<String, Object>>();
+        if (listmap == null || listmap.size() == 0)
+            return r;
+        for (Map<String, Object> map : listmap) {
+            r.add(keyToLower(map));
+        }
+        return r;
+    }
+
+
+
+    /**
+     * 把map对象的key全部转为小写形式
+     *
+     * @param map
+     * @return
+     */
+    public static Map<String, Object> keyToLower(Map<String, Object> map) {
+        Map<String, Object> r = new HashMap<String, Object>();
+        if (map == null || map.size() == 0)
+            return r;
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            r.put(entry.getKey().toLowerCase(), entry.getValue());
+        }
+        return r;
     }
 
     /**
